69d70e0bbf487e3215f189849c3c09fe339108c9
trungsliver <buiductrung.cva@gmail.com>
2025-07-20 12:03:45 +0700

    Lesson 5

---
+++ Lesson5/cp1.py
@@ -0,0 +1,14 @@
+# Quy tắc đặt tên folder: [TenLop]_[HoTen]_CP1
+# VD: PTA22_DucTrung_CP1
+
+# Yêu cầu: trong folder có 2 file: 
+    # main.py 
+    # oop.py
+
+# Link đề bài: shorturl.at/4hR0X
+
+# Hạn nộp: 11h15
+
+
+
+
